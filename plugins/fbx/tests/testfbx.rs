fca518f14eed8f3ad55c7cd6a6913e42b495ac68
Kent <642238553@qq.com>
2022-06-19 01:34:08 +0800

    test seems ok, but not that perfect

--- plugins/fbx/tests/testfbx.rs
+++ plugins/fbx/tests/testfbx.rs
@@ -1,10 +1,22 @@
+pub mod tools;
+
+use tools::*;
+
 #[cfg(test)]
 mod tests {
     use fbx::fileToScene;
+    use scene_file::*;
+    use tools::PrintSelf;
+    use crate::tools;
 
     #[test]
     fn it_works() {
         println!("{:?}", std::env::current_dir());
-        fileToScene(String::from("samplefbx/source/scene.fbx"));
+        let ret = fileToScene(String::from("samplefbx/source/scene.fbx"));
+        if let Some(data) = ret {
+            let d = data.as_slice().clone();
+            let s = scene::get_root_as_scene(d);
+            s.root().unwrap().children().unwrap().get(0).children().unwrap().get(0).children().unwrap().len().show_self();
+        }
     }
 }
